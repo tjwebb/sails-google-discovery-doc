e772023a003723ac1133995e03a51d2a60c03228
Travis Webb <me@traviswebb.com>
2014-08-08 12:57:47 -0400

    initial implementation; untested

---
+++ test.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+var api = require('xtuple-api');
+
+describe('sails-google-discovery-doc', function () {
+  var SailsDiscovery = require('./');
+
+  describe('#createRestDescription', function () {
+
+  });
+
+  describe('#createDirectoryList', function () {
+
+  });
+
+});
