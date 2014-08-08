b1b0e72d818c09bb3848727dc1d40b1431d67e28
Travis Webb <me@traviswebb.com>
2014-08-08 13:20:06 -0400

    test suite setup, enabled travis-ci

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 sails-google-discovery-doc
 ==========================
 
+[![Build Status](https://travis-ci.org/tjwebb/sails-google-discovery-doc.svg)](https://travis-ci.org/tjwebb/sails-google-discovery-doc)
+
 Generate a Google API Discovery Document for a Sails.js Application
--- test.js
+++ test.js
@@ -1,10 +1,48 @@
 var assert = require('assert');
-var api = require('xtuple-api');
+var pkg = require('xtuple-api/package');
+var DiscoveryDocument = require('google-discovery-document');
 
 describe('sails-google-discovery-doc', function () {
+  var log = console.log;
+  var error = console.error;
+  console.log = function () { };
+  console.error = function () { };
+  var api = require('xtuple-api');
+
   var SailsDiscovery = require('./');
 
+  before(function (done) {
+    this.timeout(10000);
+
+    var interval = setInterval(function () {
+      if (global.sails) {
+        clearInterval(interval);
+        global.sails.once('lifted', function () {
+          sails.config.log.level = 'error';
+          console.log = log;
+          console.error = error;
+          done();
+        });
+      }
+    }, 1);
+
+  });
+
   describe('#createRestDescription', function () {
+    var doc;
+
+    before(function () {
+      doc = SailsDiscovery.createRestDescription(sails, pkg);
+    });
+
+    it.skip('should pass google-discovery-document validation', function () {
+      console.log(doc);
+      DiscoveryDocument.validate(doc);
+    });
+
+    it.skip('should contain correct preamble', function () {
+
+    });
 
   });
 
